72bad96cffea05db691d0a3fab33bef7524397cd
Sentrosi-Git <bonny.sentrosi@gmail.com>
2018-04-23 12:45:50 +1000

    Made changes to styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
 
-  get 'home/dashboard'
-
-  get 'home/account'
-
-  get 'home', to: 'home#index', as: 'home'
-  get 'dashboard', to: 'home#dashboard', as: 'dashboard'
-  get 'account', to: 'home#account', as: 'account'
-  root 'home#index'
 
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   root :to => 'pages#home'
