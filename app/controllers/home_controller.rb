a79f9bb848662bdeb371d3752fd4e92574cccc30
Sentrosi-Git <bonny.sentrosi@gmail.com>
2018-04-23 11:30:34 +1000

    changes some routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,10 @@
-
 class HomeController < ApplicationController
-
   def index
-    set_tab :home
-    render index
   end
 
   def dashboard
-    set_tab: dashboard
-    render index
   end
 
   def account
-    set_tab: account
-    render index
   end
-
 end
