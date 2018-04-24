20d4cf1d0d4114c5ea96deef89ae339b82b0b932
James Kim <jameskim@Jamess-MacBook-Pro-2.local>
2018-04-24 11:31:28 +1000

    update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,14 @@
 class GamesController < ApplicationController
   before_action :set_game, only: [:show, :edit, :update, :destroy]
   set_tab :games
+
   # GET /games
   # GET /games.json
   def index
     @games = Game.all
   end
+
+
 
   # GET /games/1
   # GET /games/1.json
