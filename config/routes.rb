e295ea36bb4ca740f2b33ec08d5444d2ceabe89e
James Kim <jameskim@Jamess-MBP-2.gateway>
2018-04-26 02:38:10 +1000

    livechat

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,12 @@ Rails.application.routes.draw do
   resources :users
   resources :events
   resources :games
-  resources :charges 
+  resources :charges
+  resources :chat_rooms, only: [:new, :create, :show, :index]
+
+  mount ActionCable.server => '/cable'
+
+  root 'chat_rooms#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
