69b0359803a31b40e1d6bd3ac71368adc0f83bc5
James Kim <jameskim@Jamess-MBP-2.gateway>
2018-04-27 00:38:17 +1000

    update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -44,8 +44,7 @@ class GamesController < ApplicationController
     @age = game["boardgames"]["boardgame"]["age"]
     @playing_time = game["boardgames"]["boardgame"]["playingtime"]
     # return game
-
-    Game.find('boardgame.name' => @name)
+    list = Game.where('boardgame.name' => @name)
       # redirect them to the show page for that boardgame so they can add that game to their shelf from there
         @game = Game.create(:name => @name, :description => @description, :min_players => @min_players, :max_players => @max_players, :age => @age, :playing_time => @playing_time)
   end
