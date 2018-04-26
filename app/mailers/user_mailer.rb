da3568ab805de8d3e6c96922fa2e08bc911829f1
Abdulmajeed Yusuf <oolakunle@ymail.com>
2018-04-27 09:57:00 +1000

    prelim sendgrid

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+
+  default from: "info@boardgamesherpa.com"
+
+  def welcome(user)
+    @user = user #@user will be whatever user we pass in to the 'welcome' method
+    mail( :to => @user.email, :subject => "Welcome to Board Game Sherpa!" )
+  end
+
+end
