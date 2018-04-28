ae035d40d745b7261319eeef77a68c91c5c406e7
Abdulmajeed Yusuf <oolakunle@ymail.com>
2018-04-28 10:41:28 +1000

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,14 +1,14 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { :host => 'your.websitedomain.com' }
+  config.action_mailer.default_url_options = { :host => 'sherpa-app.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
    :address        => "smtp.sendgrid.net",
    :port           => "25",
    :authentication => :plain,
    :user_name      => ENV['SENDGRID_USERNAME'],
    :password       => ENV['SENDGRID_PASSWORD'],
-   :domain         => ENV['SENDGRID_DOMAIN']
+   :domain         => ENV['herokuapp.com']
   }
 
 
